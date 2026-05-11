2c4f57caccadd68ce94bb1e52defd08279359280
Muzzayan Omaar <muzzayanomaar@gmail.com>
2026-05-11 16:11:29 +0400

    Added insight for visitor tracking2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 import Admin from "./pages/Admin";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
